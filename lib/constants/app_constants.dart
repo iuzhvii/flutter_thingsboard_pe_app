3c82a15e40bd195b939fef7af211107360453e44
irynauzhviy <iuzhvii@thingsboard.io>
2024-11-25 15:36:52 +0200

    Old self

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,11 +1,11 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsBoardApiEndpoint = 'https://pe.tbqa.cloud';
   static const thingsboardOAuth2CallbackUrlScheme =
       'org.thingsboard.pe.app.auth';
 
   /// Not for production (only for debugging)
-  static const thingsboardOAuth2AppSecret = 'Your app secret here';
+  static const thingsboardOAuth2AppSecret = 'cnVuTFJFbGR1aDhqaWNOTHJJb2ZEN1BoV0cxZXkxWXVVQ2ZhR2Y0cUowM2IzcHdGRElwaDJFcXBsanh5eFVUVg==';
 
   /// Not for production (only for debugging)
-  static const thingsboardSignUpAppSecret = 'Your sign up app secret here';
+  static const thingsboardSignUpAppSecret = 'd8ZmRJKbMgAReUv4srQi6ut4';
 }
